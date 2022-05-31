dff97ef19b5644d314c97de62f95749cb9490391
Jeason Campos <camposjeason@gmail.com>
2022-05-31 12:18:15 -0400

    meta-tags

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,6 +12,9 @@ export default function Home() {
     <Head>
       <title>Coming Soon</title>
       <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Coming Soon" />
+        <meta name="keywords" content="next, create next app, create next, next app, next ui, next ui org, next ui org" />
+        <meta name="author" content="JECB" />
     </Head>
 
       <Grid justify='center' alignItems='center' alignContent='center' css={{ h: '100vh', w: '100%', bg: 'rgb(234,230,160)', d: 'flex' }}>
